e03a12442d3c410f2139d3f114853446254d21fa
TheAustrian1998 <theaustrianeth@gmail.com>
2022-07-26 09:50:26 -0300

    fix: slither warnings

--- solidity/ExpirableAirdrop.sol
+++ solidity/ExpirableAirdrop.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+pragma solidity 0.8.7;
 
 import './utils/Governable.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 
 /// @title ExpirableAirdrop
 /// @notice airdrop claimable by members of a merkle tree with expiration date
 /// @author Mean Finance (based on: https://github.com/Anish-Agnihotri/merkle-airdrop-starter/blob/master/contracts/src/MerkleClaimERC20.sol)
 contract ExpirableAirdrop is Governable {
+  using SafeERC20 for IERC20;
+
   IERC20 public immutable token;
   uint256 public immutable expirationTimestamp;
   bytes32 public immutable merkleRoot;
@@ -24,16 +27,16 @@ contract ExpirableAirdrop is Governable {
   event Retrieved(uint256 _timestamp, address _receiver);
 
   /// @notice Creates a new ExpirableAirdrop contract
-  /// @param _governor governor address
+  /// @param _govAddy governor address
   /// @param _token token to airdrop
   /// @param _expirationTimestamp expiration timestamp
   /// @param _merkleRoot of claimees
   constructor(
-    address _governor,
+    address _govAddy,
     IERC20 _token,
     uint256 _expirationTimestamp,
     bytes32 _merkleRoot
-  ) Governable(_governor) {
+  ) Governable(_govAddy) {
     token = _token;
     expirationTimestamp = _expirationTimestamp;
     merkleRoot = _merkleRoot;
@@ -56,7 +59,7 @@ contract ExpirableAirdrop is Governable {
     hasClaimed[_claimee] = true;
 
     // INTERACTIONS
-    token.transfer(_receiver, _amount);
+    token.safeTransfer(_receiver, _amount);
 
     emit Claimed(_claimee, _receiver, _amount);
   }
@@ -89,7 +92,7 @@ contract ExpirableAirdrop is Governable {
   /// @param _amount of tokens to deposit
   function depositTokens(uint256 _amount) external {
     // INTERACTIONS
-    token.transferFrom(msg.sender, address(this), _amount);
+    token.safeTransferFrom(msg.sender, address(this), _amount);
 
     emit Deposited(_amount);
   }
@@ -100,9 +103,9 @@ contract ExpirableAirdrop is Governable {
     if (block.timestamp <= expirationTimestamp) revert NotExpired();
 
     // INTERACTIONS
-    address _governor = IGovernable(address(this)).governor();
-    token.transfer(_governor, token.balanceOf(address(this)));
+    address _gov = IGovernable(address(this)).governor();
+    token.safeTransfer(_gov, token.balanceOf(address(this)));
 
-    emit Retrieved(block.timestamp, _governor);
+    emit Retrieved(block.timestamp, _gov);
   }
 }
