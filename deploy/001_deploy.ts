dc42559740123fa3204fa38bf6e037b54e327584
TheAustrian <theaustrianeth@gmail.com>
2022-08-05 08:22:31 -0300

    feat: add deploy script and prepare for deploy (#4)
    * feat: add deploy script and prepare for deploy
    
    * fix: typo

--- deploy/001_deploy.ts
+++ deploy/001_deploy.ts
@@ -1,32 +1,59 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { getChainId, shouldVerifyContract } from '../utils/deploy';
+import { shouldVerifyContract } from '../utils/deploy';
 
-export const INITIAL_GREET: { [chainId: string]: string } = {
-  '1': 'Halo!',
-  '137': 'Halo to polygon network!',
-  '31337': 'Hello',
-};
+const oneMonth: number = 30 * 24 * 60 * 60; // one month in seconds
+
+let governor: string; // governor address
+let token: string; // token address
+let monthsTillExpiration: number; // how many months to expiration (since deploy block timestamp)
+let merkleRoot: string; // merkle root in hex string
 
 const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
 
-  const chainId = await getChainId(hre);
+  const network = hre.network.name;
+  switch (network) {
+    case 'hardhat':
+      governor = deployer;
+      token = hre.ethers.utils.hexlify(hre.ethers.utils.randomBytes(20));
+      monthsTillExpiration = 3;
+      merkleRoot = hre.ethers.utils.hexlify(hre.ethers.utils.randomBytes(32));
+      break;
+    case 'goerli':
+      governor = deployer;
+      token = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
+      monthsTillExpiration = 3;
+      merkleRoot = hre.ethers.utils.hexlify(hre.ethers.utils.randomBytes(32));
+      break;
+    case 'optimism':
+      governor = '';
+      token = '';
+      monthsTillExpiration = 3;
+      merkleRoot = '';
+      break;
+  }
 
-  const deploy = await hre.deployments.deploy('Greeter', {
-    contract: 'solidity/contracts/Greeter.sol:Greeter',
+  const nowTimestamp = Math.round(Date.now() / 1000);
+  const helperArgs = [governor, token, monthsTillExpiration * oneMonth + nowTimestamp, hre.ethers.utils.arrayify(merkleRoot)];
+
+  // deploy
+  const deploy = await hre.deployments.deploy('ExpirableAirdrop', {
+    contract: 'solidity/ExpirableAirdrop.sol:ExpirableAirdrop',
     from: deployer,
-    args: [INITIAL_GREET[chainId]],
+    args: helperArgs,
     log: true,
   });
 
-  if (await shouldVerifyContract(deploy)) {
+  // verify
+  if (hre.network.name !== 'hardhat' && (await shouldVerifyContract(deploy))) {
     await hre.run('verify:verify', {
       address: deploy.address,
-      constructorArguments: [INITIAL_GREET[chainId]],
+      constructorArguments: helperArgs,
     });
   }
 };
+
 deployFunction.dependencies = [];
-deployFunction.tags = ['Greeter'];
+deployFunction.tags = ['ExpirableAirdrop'];
 export default deployFunction;
